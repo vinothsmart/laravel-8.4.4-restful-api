2264a2d32c6215f5d36b943b945ad0e7cdea5dc1
vinothsmart <s.vinothkanna90@gmail.com>
2021-01-06 16:07:39 +0530

    Exception Handler updated

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,6 +2,7 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
