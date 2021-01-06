53400747d4a1c5c3f420b1641ac9a15bf60b48c3
vinothsmart <s.vinothkanna90@gmail.com>
2021-01-06 15:45:30 +0530

    role model updated

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Role extends Model
 {
     use HasFactory;
+
+    use SoftDeletes;
+
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role', 'client_details',
+    ];
 }
