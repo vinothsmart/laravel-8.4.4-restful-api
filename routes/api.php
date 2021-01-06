e847b9daee837db20448084ab53cf440806cd383
vinothsmart <s.vinothkanna90@gmail.com>
2021-01-06 15:55:00 +0530

    added routes for roles

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,14 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
+ */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * Roles
+ */
+
+Route::resource('roles', 'Role\RoleController', ['except' => ['create', 'edit']]);
