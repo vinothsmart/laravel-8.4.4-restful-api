49e0ffe64e589c85c8d1787df0ef7a20971cdbb5
vinothsmart <s.vinothkanna90@gmail.com>
2021-02-26 14:53:26 +0530

    add view and update user

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -20,16 +20,6 @@ class UserController extends ApiController
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -37,7 +27,61 @@ class UserController extends ApiController
      */
     public function store(Request $request)
     {
-        //
+        /**
+         * Validation
+         */
+        $rules = [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6|confirmed',
+            'role_id' => 'required',
+            // 'image' => 'required',
+        ];
+
+        $this->validate($request, $rules);
+
+        $data = $request->all();
+
+        /**
+         * Role based condition
+         */
+        // Here we get hashids
+        $encryptedRoleId = $request->role_id;
+
+        // Decrypyt Role Id
+        $decryptedRoleId = \Hashids::connection(\App\Role::class)->decode($encryptedRoleId);
+        $roleId = $decryptedRoleId[0];
+
+        if ($roleId == 1 || $roleId == 2) {
+            $isAdmin = true;
+        } else {
+            $isAdmin = false;
+        }
+        $data['password'] = bcrypt($request->password);
+
+        if ($request->file('image') == null) {
+            $data['image'] = "default.jpeg";
+        } else {
+            $data['image'] = $request->file('image')->store('');
+        }
+
+        $data['email_verified_at'] = $isAdmin == true ? now() : null;
+        $data['verified'] = $isAdmin == true ? User::VERIFIED_USER : User::UNVERIFIED_USER;
+        $data['verification_token'] = $isAdmin == true ? null : User::generateVerificationCode();
+        $data['admin'] = $isAdmin == true ? User::ADMIN_USER : User::REGULAR_USER;
+        $data['client_details'] = $this->applicationDetector();
+
+        $user = User::create($data);
+
+        // Adding to Pivot Table
+        $userRoleAssign = ['role_id' => $roleId, 'user_id' => $user->id];
+
+        DB::table('role_user')
+            ->insert($userRoleAssign);
+
+        $user->roles;
+
+        return $this->showOne($user, 201);
     }
 
     /**
@@ -48,18 +92,7 @@ class UserController extends ApiController
      */
     public function show($id)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return $this->showOne($user);
     }
 
     /**
@@ -71,7 +104,92 @@ class UserController extends ApiController
      */
     public function update(Request $request, $id)
     {
-        //
+        /**
+         * Validation
+         */
+        $rules = [
+            'email' => 'email|unique:users,email,' . $user->id,
+            'password' => 'min:6|confirmed',
+            'admin' => 'in:' . User::ADMIN_USER . ',' . User::REGULAR_USER,
+        ];
+
+        $this->validate($request, $rules);
+
+        /**
+         * Role based condition
+         */
+        if ($request->has('role_id')) {
+            // Here we get hashids
+            $encryptedRoleId = $request->role_id;
+
+            // Decrypyt Role Id
+            $decryptedRoleId = \Hashids::connection(\App\Role::class)->decode($encryptedRoleId);
+            $roleId = $decryptedRoleId[0];
+        } else {
+            $roleOfuser = DB::table('role_user')->where('user_id', $user->id)->first();
+            $roleId = $roleOfuser->role_id;
+        }
+
+        if ($roleId == 1 || $roleId == 2) {
+            $isAdmin = true;
+        } else {
+            $isAdmin = false;
+        }
+
+        if ($request->has('name')) {
+            $user->name = $request->name;
+        }
+
+        if ($request->has('email') && $user->email != $request->email) {
+            $user->verified = $isAdmin == true ? User::VERIFIED_USER : User::UNVERIFIED_USER;
+            $user->verification_token = $isAdmin == true ? null : User::generateVerificationCode();
+            $user->email = $request->email;
+        }
+
+        if ($request->has('password')) {
+            $user->password = bcrypt($request->password);
+        }
+
+        if ($request->hasFile('image')) {
+            // Delete old image
+            Storage::delete($user->image);
+            $user->image = $request->file('image')
+                ->store('');
+        }
+
+        if ($request->has('admin')) {
+
+            $this->allowedAdminAction();
+
+            if (!$user->isAdmin()) {
+                return $this->errorResponse('Only Admin users can modify the admin field', 409);
+            }
+            $user->admin = $isAdmin == true ? $request->admin : User::REGULAR_USER;
+        }
+
+        // Update role
+        if ($request->has('role_id')) {
+            $userRoleAssign = ['role_id' => $roleId];
+            DB::table('role_user')
+                ->where('user_id', $user->id)
+                ->update($userRoleAssign);
+
+            // Getting Client Details
+            $user->client_details = $this->applicationDetector();
+        }
+
+        if (!$user->isDirty()) {
+            return $this->errorResponse('You need to specify a different value to update', 422);
+        }
+
+        // Getting Client Details
+        $user->client_details = $this->applicationDetector();
+
+        $user->save();
+
+        $user->roles;
+
+        return $this->showOne($user);
     }
 
     /**
