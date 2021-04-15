4a5e73c10d6e37f141ef654e8f6a9db0ca9feb94
vinothsmart <s.vinothkanna90@gmail.com>
2021-04-15 09:46:44 +0530

    delete role

--- app/Http/Controllers/Role/RoleController.php
+++ app/Http/Controllers/Role/RoleController.php
@@ -101,11 +101,13 @@ class RoleController extends ApiController
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  int  $role
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Role $role)
     {
-        //
+        $role->delete();
+
+        return $this->showOne($role);
     }
 }
