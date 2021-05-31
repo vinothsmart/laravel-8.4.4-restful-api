c38c85d5e342efd951e900eee9444e7d53e6e88e
vinothsmart <s.vinothkanna90@gmail.com>
2021-05-31 15:18:40 +0530

    Users hateoas hypermedia

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,7 +22,7 @@ class UserSeeder extends Seeder
         // Clear the user events
         User::flushEventListeners();
 
-        User::factory()->create(['name' => 'Vino', 'email' => 'vino@ab.com', 'password' => '$2y$10$1KeHVDk4jl1LU7psksi9iet369iU7eli4fshl/zXcIxQqKCLMTZSW', 'image' => 'default.jpg', 'verified' => 1, 'admin' => User::ADMIN_USER, 'verification_token' => null]);
+        User::factory()->create(['name' => 'Vino', 'email' => 'vino@ab.com', 'password' => '$2y$10$1KeHVDk4jl1LU7psksi9iet369iU7eli4fshl/zXcIxQqKCLMTZSW', 'image' => 'default.jpeg', 'verified' => 1, 'admin' => User::ADMIN_USER, 'verification_token' => null]);
 
         $usersQunatity = 10;
         User::factory()->count($usersQunatity)->create();
